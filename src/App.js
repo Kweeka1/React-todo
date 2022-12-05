c058b32254e70afe70c69d35edd303f5af9040be
Kweeka <mtalbi29@yahoo.com>
2022-12-05 14:41:35 +0100

    Update App.js, add child components and react router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
+import { List } from 'react-bootstrap-icons';
+import Sidebar from './Components/Sidebar/Sidebar';
+import Todo from './Pages/Todo/Todo';
+import ErrorPage from './Pages/Error/Error';
+import About from './Pages/About/About';
 
 function App() {
+  const [isSidebarActive, toggleSidebarActive] = useState(true);
+
+  const handleSidebarClose = () => {
+    toggleSidebarActive((prevState) => !prevState);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-wrapper">
+      <BrowserRouter>
+        {
+        isSidebarActive
+          ? (<Sidebar handleSidebarClose={handleSidebarClose} />)
+          : (<List onClick={handleSidebarClose} className="sidebar-open" size={62} fill="#000" />)
+      }
+        <Routes>
+          <Route path="/" element={<Todo />} />
+          <Route path="/about/author" element={<About />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
